aa05b0cd2b600e816aea1b88082deb8dace82e06
mojvaf <mvafa44@gmail.com>
2020-09-15 10:44:15 +0200

    Updates

---
+++ precache-manifest.f3d840ccd463218e8f0feaf97178687f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa97d3c28f16c25b842584a9fe4bf12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e069ac05e9feda4c4b61",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7ca6f4222a2f91751a24",
+    "url": "./static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "e069ac05e9feda4c4b61",
+    "url": "./static/js/2.de4eb302.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.de4eb302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca6f4222a2f91751a24",
+    "url": "./static/js/main.963ab59a.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
